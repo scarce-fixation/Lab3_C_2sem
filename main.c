18a140db39c88618717658a8d8f26db25beaa39b
Bakhmutov_Ivan <vanya.bahmutov@yandex.ru>
2026-04-29 11:23:16 +0700

    Added func to set k's bit

--- main.c
+++ main.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+void set1(unsigned char* vec, size_t bits, size_t k) {
+    if (vec && (k > 0 && k < bits)) {
+        int byte = k / 8;
+        int bit = k % 8;
+        unsigned char mask = 1;
+        mask = mask << 8 - bit;
+        vec[byte] = vec[byte] | mask;
+    }
+}
+
+void set0(unsigned char* vec, size_t bits, size_t k) {
+    if (vec && (k > 0 && k < bits)) {
+        int byte = k / 8;
+        int bit = k % 8;
+        unsigned char mask = 1;
+        mask = mask << 8 - bit;
+        mask = ~mask;
+        vec[byte] = vec[byte] & mask;
+    }
+}
 
 int main()
 {
