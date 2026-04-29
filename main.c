bbb017586b08749fcfd565ef385f15f6f328a676
Bakhmutov_Ivan <vanya.bahmutov@yandex.ru>
2026-04-29 10:58:42 +0700

    Added func logMul

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+unsigned char* logMul(unsigned char* vecA, size_t bitsA, unsigned char* vecB, size_t bitsB) {
+    if (vecA == NULL && vecB) return vecB;
+    if (vecB == NULL && vecA) return vecA;
+    if ((vecA == NULL && vecB == NULL) || (bitsA != bitsB)) return NULL;
+
+    int cells = ((bitsA - 1) / 8) + 1;
+    unsigned char* res = (unsigned char*)calloc(cells, sizeof(unsigned char));
+
+    for (int i = 0; i < cells; i++) res[i] = vecA[i] & vecB[i];
+    return res;
+}
 
 int main()
 {
