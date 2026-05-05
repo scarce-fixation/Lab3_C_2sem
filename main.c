6c6093796c9d190817a6c4737b8310610d4e32e3
Bakhmutov_Ivan <vanya.bahmutov@yandex.ru>
2026-05-05 16:44:32 +0700

    Added function inversion

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
-
+void inversion(unsigned char* vec, size_t bits) {
+    if (!vec) return;
+    int cells = (bits - 1) / 8 + 1;
+    for (int i = 0; i < cells; i++) {
+        unsigned char result = 0;
+        for (int j = 0; j < 8; j++) {
+            result = (result << 1) | (vec[i] & 1);
+            vec[i] >>= 1;
+        }
+        vec[i] = ~result;
+    }
+    int tailBits = bits % 8;
+    vec[cells - 1] >>= (8 - tailBits);
+}
 int main()
 {
-    printf("Hello World!\n");
     return 0;
 }
