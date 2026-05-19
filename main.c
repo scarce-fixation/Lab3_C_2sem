40d98245f7283c95c544b84f8e8f018161acf60e
Bakhmutov_Ivan <vanya.bahmutov@yandex.ru>
2026-05-19 11:26:36 +0700

    Added func printBV

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-
+#include<stdlib.h>
+#define vecMaxSize 1000
+void printBV(unsigned char* vec, size_t cells) {
+    if (vec && cells < vecMaxSize) {
+        char* str = convertBvToStr(vec, cells);
+        if (str) {
+            printf("%s\n", str);
+            free(str);
+        }
+    }
+}
 int main()
 {
     printf("Hello World!\n");
