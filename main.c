cc84903b693a9213300d17fa7587ee56ad26dd8d
Bakhmutov_Ivan <vanya.bahmutov@yandex.ru>
2026-05-19 13:28:06 +0700

    Added test cases. Fixed tail inversion

--- main.c
+++ main.c
@@ -11,9 +11,33 @@ void inversion(unsigned char* vec, size_t bits) {
         vec[i] = ~result;
     }
     int tailBits = bits % 8;
-    vec[cells - 1] >>= (8 - tailBits);
+    if(tailBits != 0) vec[cells - 1] >>= (8 - tailBits);
 }
 int main()
 {
+    //normal inversion
+    //char str[] = "11110000";
+    //char str[] = "08283490jfij4r891092491243";
+    //char str[] = "87123908jc1309123=cx1zk121s12312392834ucu0j3134u1092cu4j3c8u124cu9ghfugoiug8urg8egaerg";
+    //char str[] = "11111111111111111111000000000000000000000000000";
+    //char str[] = "1010001111110000";
+    //char str[] = "11110000";
+    //char str[] = "11110000";
+    //char str[] = "11110000";
+    //tail inversion
+    char str[] = "111100011011";
+    int cells = 0;
+    int bits = strlen(str);
+    unsigned char* vec = convertStrToLongBV(str, &cells);
+    printf("Original: ");
+    printBV(vec, cells);
+    //vec is null
+    //inversion(NULL, bits);
+    //bits is negative size_t 64 bits
+    //inversion(vec, -1000);
+    inversion(vec, bits);
+    printf("Inverted: ");
+    printBV(vec, cells);
     return 0;
 }
+
